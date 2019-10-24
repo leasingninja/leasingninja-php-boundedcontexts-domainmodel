f3e52fbdd4727b1fd88fa53fe04429c23972cca7
Henning Schwentner <hs@wps.de>
2019-10-24 23:06:32 +0200

    Added var comments

--- tests/LeasingNinja/Sales/Domain/AmountTest.php
+++ tests/LeasingNinja/Sales/Domain/AmountTest.php
@@ -10,7 +10,9 @@ class AmountTest extends TestCase
     public function givenTwoEqualAmounts_whenEquals_thenAreEqual(): void
     {
         // given
+        /** @var Amount $amount1 */
         $amount1 = Amount::of(100, "EUR");
+        /** @var Amount $amount2 */
         $amount2 = Amount::of(100, "EUR");
 
         // when
