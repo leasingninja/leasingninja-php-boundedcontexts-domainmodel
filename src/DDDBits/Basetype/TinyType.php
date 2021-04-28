9846527fcf5009379252981abdd6f2dfc7d2d847
Henning Schwentner <hs@wps.de>
2021-04-28 23:53:26 +0200

    Temporarily add DDDBits/TinyType

---
+++ src/DDDBits/Basetype/TinyType.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace DDDBits\Basetype;
+
+
+abstract class TinyType
+{
+    private $value;
+
+	protected function __construct($value)
+    {
+		$this->value = $value;
+	}
+
+    public function value()
+    {
+		return $this->value;
+	}
+
+	public function __toString(): string
+    {
+		return TinyType::class + " [" + value() + "]";
+	}
+
+	public function equals(TinyType $other): bool
+    {
+        return $this->value === $other->value;
+	}
+
+}
