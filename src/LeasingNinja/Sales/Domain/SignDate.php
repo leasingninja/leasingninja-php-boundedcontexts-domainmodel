0e5110ef6f2ccd7b91e14a35de8070bcf8e06b86
Henning Schwentner <hs@wps.de>
2022-10-20 18:46:53 +0200

    Update SignDate.php

--- src/LeasingNinja/Sales/Domain/SignDate.php
+++ src/LeasingNinja/Sales/Domain/SignDate.php
@@ -17,6 +17,7 @@ class SignDate extends TinyType
     public static function of(int $year, int $month, int $day): SignDate
     {
         $date = new DateTimeImmutable();
+        $date = $date->setTime(0, 0);
         return new SignDate($date->setDate($year, $month, $day));
     }
 }
