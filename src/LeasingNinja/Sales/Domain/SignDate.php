2450cf027202d3d4de69ba104adc2867d86e6c8b
Henning Schwentner <hs@wps.de>
2023-10-27 17:38:11 +0200

    Remove TinyType from SignDate

--- src/LeasingNinja/Sales/Domain/SignDate.php
+++ src/LeasingNinja/Sales/Domain/SignDate.php
@@ -5,19 +5,26 @@ declare(strict_types=1);
 namespace LeasingNinja\Sales\Domain;
 
 use DateTimeImmutable;
-use DDDBits\Basetype\TinyType;
+
 use PHPMolecules\DDD\Attribute\ValueObject;
 
-/**
- * @extends TinyType<DateTimeImmutable>
- */
 #[ValueObject]
-class SignDate extends TinyType
+class SignDate
 {
+    public function __construct(
+        private readonly DateTimeImmutable $dateTime
+    ) {}
+
     public static function of(int $year, int $month, int $day): SignDate
     {
         $date = new DateTimeImmutable();
         $date = $date->setTime(0, 0);
         return new SignDate($date->setDate($year, $month, $day));
+    }
+
+    public function equals(SignDate $other): bool
+    {
+        return $other != null
+            && $this->dateTime === $other->dateTime;
     }
 }
