c6c3cb9bf0b7c008dc669dac06f566653ce334f1
Henning Schwentner <hs@wps.de>
2023-10-27 19:59:55 +0200

    Add use case FilloutContract

--- tests/LeasingNinja/Sales/Application/FilloutContractTest.php
+++ tests/LeasingNinja/Sales/Application/FilloutContractTest.php
@@ -5,7 +5,53 @@ declare(strict_types=1);
 namespace LeasingNinja\Sales\Application;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 
-class FilloutContractTest extends TestCase
+use LeasingNinja\Sales\Application\FilloutContract;
+
+use LeasingNinja\Sales\Domain\Amount;
+use LeasingNinja\Sales\Domain\Car;
+use LeasingNinja\Sales\Domain\Contract;
+use LeasingNinja\Sales\Domain\ContractNumber;
+use LeasingNinja\Sales\Domain\Contracts;
+use LeasingNinja\Sales\Domain\Currency;
+use LeasingNinja\Sales\Domain\Customer;
+
+final class FilloutContractTest extends TestCase
 {
+    /*
+	@Mock
+	private Contracts $contractsMock;
+
+	@InjectMocks
+    */
+    private FilloutContract $filloutContractUnderTest;
+
+    #[Test]
+	function givenEmptyContract_WhenFillout_ThenSave(): void
+    {
+        // Prepare
+        $contractsMock = $this->createMock(Contracts::class);
+		$contractsMock->expects($this->once())
+            ->method('save')
+            ->with($this->equalTo(
+                new Contract(
+                    ContractNumber::of("4711"),
+                    Customer::of("Bob Smith"),
+                    Car::of("Mercedes Benz E-Class"),
+                    Amount::of(10_000, Currency::EUR))));
+        $filloutContractUnderTest = new FilloutContract($contractsMock);
+
+		// Given
+
+		// When
+		$filloutContractUnderTest->with(
+				ContractNumber::of("4711"),
+				Customer::of("Bob Smith"),
+				Car::of("Mercedes Benz E-Class"),
+				Amount::of(10_000, Currency::EUR));
+
+
+		// Then
+	}
 }
